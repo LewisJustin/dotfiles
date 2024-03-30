b28b1a7cbb55f96b53e759f02ce3a8c4da3d7431
LewisJustin <justindavidlewis@hotmail.com>
2024-03-29 21:26:13 -0600

    feature: primagen based neovimrc

---
+++ lua/cc/packer.lua
@@ -0,0 +1,81 @@
+-- only loaded by calling `lua require("cc.packer")` from your init.lua
+
+-- vim.cmd.packadd("packer.nvim")
+-- 
+-- return require("packer").startup(function(use)
+--     -- Packer can manage itself
+--     use "wbthomason/packer.nvim"
+-- 
+--     use {
+--         'nvim-telescope/telescope.nvim', tag = '0.1.5',
+--         requires = { {'nvim-lua/plenary.nvim'} }
+--     }
+-- 
+--     use({
+--         "catppuccin/nvim",
+--         as="catppuccin",
+--         config = function()
+--             vim.cmd.colorscheme("catppuccin-mocha")
+--         end
+--     })
+-- 
+--     use({
+--         "folke/trouble.nvim",
+--         config = function()
+--             require("trouble").setup {
+--                 icons = false,
+--             }
+--         end
+--     })
+-- 
+--     use({
+--         "nvim-treesitter/nvim-treesitter",
+--         run = function()
+--             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+--             ts_update()
+--         end,
+--     })
+--     use({ "nvim-treesitter/playground" })
+-- 
+--     use({ "theprimeagen/harpoon" })
+--     use({ "mbbill/undotree" })
+-- 
+--     use({ "tpope/vim-fugitive" })
+-- 
+--     use({
+--         'VonHeikemen/lsp-zero.nvim',
+--         branch = 'v3.x',
+--         requires = {
+--             -- LSP Support
+--             {'neovim/nvim-lspconfig'},
+--             {'williamboman/mason.nvim'},
+--             {'williamboman/mason-lspconfig.nvim'},
+-- 
+--             -- Autocompletion
+--             {'hrsh7th/nvim-cmp'},
+--             {'hrsh7th/cmp-buffer'},
+--             {'hrsh7th/cmp-path'},
+--             {'saadparwaiz1/cmp_luasnip'}, -- enable luasnip with cmp
+--             {'hrsh7th/cmp-nvim-lsp'},
+--             {'hrsh7th/cmp-nvim-lua'},
+-- 
+--             -- Snippets
+--             {'L3MON4D3/LuaSnip'},
+--             {"rafamadriz/friendly-snippets"},
+--         },
+--     })
+-- 
+--     use {
+--         "Exafunction/codeium.vim",
+--         requires = {
+--             "nvim-lua/plenary.nvim",
+--             "hrsh7th/nvim-cmp",
+--         },
+--         -- config = function()
+--             -- require("codeium").setup({
+--             -- })
+--         -- end
+--     }
+-- 
+-- end)
+-- 
